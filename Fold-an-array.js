1e80f18b01174d4ff7de5e14d55e7c66e57d52cf
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-09 11:02:36 -0800

    Update Fold-an-array.js

--- Fold-an-array.js
+++ Fold-an-array.js
@@ -31,3 +31,9 @@ The input array should not be modified!
 
 
 // Solution
+
+function foldArray(a, n) {
+  const r = [], c = a.slice();
+  while (c.length) r.push(c.pop() + (c.shift() || 0));
+  return n - 1 ? foldArray(r, n - 1) : r;
+}
